6859cc91178a60f3db0e28f822c9b2071ab72a18
Lebedevsd <lebedevsd@gmail.com>
2013-12-23 22:20:03 +0400

    added raiting pop up

---
+++ src/com/lebedevsd/timelapse/activities/AppRater.java
@@ -0,0 +1,91 @@
+package com.lebedevsd.timelapse.activities;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class AppRater {
+	private final static String APP_TITLE = "YOUR-APP-NAME";
+	private final static String APP_PNAME = "YOUR-PACKAGE-NAME";
+
+	private final static int EVENTS_UNTIL_PROMPT = 3;
+
+	public static void app_event(Context mContext) {
+		SharedPreferences prefs = mContext.getSharedPreferences("apprater", 0);
+		if (prefs.getBoolean("dontshowagain", false)) {
+			return;
+		}
+
+		SharedPreferences.Editor editor = prefs.edit();
+
+		// Increment launch counter
+		long events_count = prefs.getLong("events_count", 0) + 1;
+		editor.putLong("events_count", events_count);
+
+		// Wait at least n events before opening
+		if (events_count >= EVENTS_UNTIL_PROMPT) {
+			showRateDialog(mContext, editor);
+		}
+
+		editor.commit();
+	}
+
+	public static void showRateDialog(final Context mContext,
+			final SharedPreferences.Editor editor) {
+		final Dialog dialog = new Dialog(mContext);
+		dialog.setTitle("Rate " + APP_TITLE);
+
+		LinearLayout ll = new LinearLayout(mContext);
+		ll.setOrientation(LinearLayout.VERTICAL);
+
+		TextView tv = new TextView(mContext);
+		tv.setText("If you enjoy using " + APP_TITLE
+				+ ", please take a moment to rate it. Thanks for your support!");
+		tv.setWidth(240);
+		tv.setPadding(4, 0, 4, 10);
+		ll.addView(tv);
+
+		Button b1 = new Button(mContext);
+		b1.setText("Rate " + APP_TITLE);
+		b1.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				mContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri
+						.parse("market://details?id=" + APP_PNAME)));
+				dialog.dismiss();
+			}
+		});
+		ll.addView(b1);
+
+		Button b2 = new Button(mContext);
+		b2.setText("Remind me later");
+		b2.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				dialog.dismiss();
+			}
+		});
+		ll.addView(b2);
+
+		Button b3 = new Button(mContext);
+		b3.setText("No, thanks");
+		b3.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if (editor != null) {
+					editor.putBoolean("dontshowagain", true);
+					editor.commit();
+				}
+				dialog.dismiss();
+			}
+		});
+		ll.addView(b3);
+
+		dialog.setContentView(ll);
+		dialog.show();
+	}
+}
